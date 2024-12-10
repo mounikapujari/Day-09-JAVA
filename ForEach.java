9a951c76bc2681afae6d14adf5525fa5d04582e0
mounikapujari <mounikapujari2005@gmail.com>
2024-12-10 22:18:26 +0530

    Add files via upload

---
+++ ForEach.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class ForEach{
+  public static void main(String[] args) {
+    Scanner s=new Scanner(System.in);
+    String a[]={"Apple","orange","Strawberry"};
+    for(String b:a)
+    System.out.print(b+",");
+}
+}
